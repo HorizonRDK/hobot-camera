7697be153322ee488e69bb8363340edef0ad1f6f
yaqiang.li <yaqiang.li@horizon.ai>
2023-04-25 11:31:41 +0800

    feat: Add necessary programs and configuration
    Summary:
        1. Add act-server and tuning_tool
        2. Add content
    
    Signed-off-by: yaqiang.li <yaqiang.li@horizon.ai>

--- camera_configs/common/initweb.sh
+++ camera_configs/common/initweb.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-act-server --http-port=8214 --http-root=./content \
+act-server --http-port=8214 --http-root=/etc/control-tool/content \
 	--fw-acc=stream --fw-channel=chardev --fw-dev=/dev/ac_isp \
 	--hw-acc=stream --hw-buf-size=1024 --hw-channel=devmem \
 	--hw-dev=/dev/mem --hw-mem-offset=0xb3000000 --hw-mem-size=0x400000 &
